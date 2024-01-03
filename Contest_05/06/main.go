4d8c0e5effd4d209c5e120990b2865edd6c97faa
sleepyy-sheep <154451625+sleepyy-sheep@users.noreply.github.com>
2024-01-03 17:41:47 +0300

    Create main.go

---
+++ Contest_05/06/main.go
@@ -0,0 +1,20 @@
+type Cat struct {
+ alive int
+}
+
+func (c Cat) is_alive() bool {
+ if c.alive==1{
+ return true
+ }
+ return false
+}
+
+type Box struct {
+}
+
+func (b Box) open() Cat {
+ cat := Cat{}
+ cat.alive=rand.Int()%2
+ return cat
+}
+
