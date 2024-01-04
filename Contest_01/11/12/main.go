0dafabf33140f2df81b1f44b3a645282021087e4
sleepyy-sheep <154451625+sleepyy-sheep@users.noreply.github.com>
2024-01-04 04:25:43 +0300

    Create main.go

---
+++ Contest_01/11/12/main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func main() {
+    var x, count int
+    fmt.Scan(&x)
+    count=0
+    for x!=1{
+        if x%2==0{
+            x=x/2
+        }else{
+            x=3*x+1
+        }
+        count++
+    }
+    
+    fmt.Println(count)
+}
