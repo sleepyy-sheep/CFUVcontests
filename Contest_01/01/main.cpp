a0c7eae2d3e533976e300b7ce5fc00e57803d7f6
sleepyy-sheep <154451625+sleepyy-sheep@users.noreply.github.com>
2024-01-04 04:19:24 +0300

    Create main.cpp

---
+++ Contest_01/01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+	cout << (sqrt(12)) * (1 - (1 / (pow(3, 2))) + (1 / (5 * (pow(3, 2)))) - (1 / (7 * (pow(3, 3)))) + (1 / (9 * (pow(3, 4)))) - (1 / (11 * (pow(3, 5)))));
+	return 0;
+
+}
